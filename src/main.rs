d6f11bc2445e15bbf63e142a41c53fe9bd1aa5ff
HarissaHero <anis.chahed@aleriaconsulting.eu>
2023-08-23 21:06:33 +0200

    init project

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use ui::run;
+use winit::{window::Window, event::WindowEvent};
+
+pub mod ui;
+
+fn main() {
+    run();
+}
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: Window,
+}
+
+impl State {
+    async fn new(window: Window) -> Self {
+        todo!()
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    pub fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    pub fn update(&mut self) {
+        todo!()
+    }
+
+    pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
